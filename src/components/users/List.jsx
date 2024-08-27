ac9fbfed7cf1cadeb804a0c35dfbc6d09232e0ff
@facundo.veron <facundo.veron@edit.com.ar>
2024-08-27 20:57:04 -0300

    Maqueta del cambio de roles para los usuarios

--- src/components/users/List.jsx
+++ src/components/users/List.jsx
@@ -1,5 +1,6 @@
 // CSS
 import "../../css/users/list.css";
+import "react-responsive-modal/styles.css";
 
 // REACT
 import { useEffect, useState } from "react";
@@ -7,10 +8,14 @@ import { useEffect, useState } from "react";
 // AXIOS
 import clientAxios from "../../config/ClientAxios";
 
+// MODAL
+import { Modal } from "react-responsive-modal";
+
 export default function List({ user }) {
     // STATES
     const [users, setUsers] = useState([]);
     const [admins, setAdmins] = useState([]);
+    const [open, setOpen] = useState(false);
 
     // EFFECTS
     useEffect(() => {
@@ -48,61 +53,111 @@ export default function List({ user }) {
         setUsers(filterUser);
     };
 
+    const onOpenModal = () => setOpen(true);
+
+    const onCloseModal = () => setOpen(false);
+
     return (
-        <div className="listUser">
-            <h1 className="listUser-title">Listado de usuarios</h1>
-            <div className="listUser-users">
-                <div className="listUser-list">
-                    <h2 className="listUser-subtitle">Administradores</h2>
-                    <table className="listUser-table">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Nombre</th>
-                                <th>Apellido</th>
-                                <th>Email</th>
-                                <th>Rol</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {admins.map((admin) => (
-                                <tr key={admin._id}>
-                                    <td>{admin._id}</td>
-                                    <td>{admin.name}</td>
-                                    <td>{admin.lastname}</td>
-                                    <td>{admin.email}</td>
-                                    <td>{admin.role.name}</td>
+        <>
+            <div className="listUser">
+                <h1 className="listUser-title">Listado de usuarios</h1>
+                <div className="listUser-users">
+                    <div className="listUser-list">
+                        <h2 className="listUser-subtitle">Administradores</h2>
+                        <table className="listUser-table">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Apellido</th>
+                                    <th>Email</th>
+                                    <th>Rol</th>
+                                    <th>Acciones</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="listUser-list">
-                    <h2 className="listUser-subtitle">Usuarios</h2>
-                    <table className="listUser-table">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Nombre</th>
-                                <th>Apellido</th>
-                                <th>Email</th>
-                                <th>Rol</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {users.map((user) => (
-                                <tr key={user._id}>
-                                    <td>{user._id}</td>
-                                    <td>{user.name}</td>
-                                    <td>{user.lastname}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.role.name}</td>
+                            </thead>
+                            <tbody>
+                                {admins.map((admin) => (
+                                    <tr key={admin._id}>
+                                        <td>{admin.name}</td>
+                                        <td>{admin.lastname}</td>
+                                        <td>{admin.email}</td>
+                                        <td>{admin.role.name}</td>
+                                        <td className="listUser-actions">
+                                            <button
+                                                className="button"
+                                                onClick={onOpenModal}
+                                            >
+                                                Cambiar Rol
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="listUser-list">
+                        <h2 className="listUser-subtitle">Usuarios</h2>
+                        <table className="listUser-table">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Apellido</th>
+                                    <th>Email</th>
+                                    <th>Rol</th>
+                                    <th>Acciones</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {users.map((user) => (
+                                    <tr key={user._id}>
+                                        <td>{user.name}</td>
+                                        <td>{user.lastname}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.role.name}</td>
+                                        <td className="listUser-actions">
+                                            <button
+                                                className="button"
+                                                onClick={onOpenModal}
+                                            >
+                                                Cambiar Rol
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div>
+                <Modal
+                    open={open}
+                    onClose={onCloseModal}
+                    center
+                    classNames={{
+                        overlay: "customOverlay",
+                        modal: "customModal",
+                        closeIcon: "customCloseIcon",
+                    }}
+                >
+                    <h2 className="listUser-modal-title">
+                        Cambiar el rol del usuario
+                    </h2>
+                    <p className="listUser-modal-paragraph">
+                        Selecciona el rol para el usuario
+                    </p>
+                    <select className="listUser-modal-select">
+                        <option className="listUser-modal-option" value="admin">
+                            Administrador
+                        </option>
+                        <option className="listUser-modal-option" value="user">
+                            Usuario
+                        </option>
+                    </select>
+                    <button className="listUser-modal-button button">
+                        Cambiar
+                    </button>
+                </Modal>
+            </div>
+        </>
     );
 }
